2d0317a84ac193220a1ce0c6b3439c2b5bff6011
Fengda HUANG <h@phodal.com>
2014-12-04 22:37:28 +0800

    add table css

--- settings.py
+++ settings.py
@@ -306,7 +306,6 @@ MIDDLEWARE_CLASSES = (
     # "mezzanine.core.middleware.SSLRedirectMiddleware",
     "mezzanine.pages.middleware.PageMiddleware",
     "mezzanine.core.middleware.FetchFromCacheMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
 )
 
 # Store these package names here as they may change in the future since
