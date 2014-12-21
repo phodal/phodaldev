83936fa94cccd4bf1e593b61dc96d0501b5aae55
Phodal <h@phodal.com>
2014-12-21 08:20:03 +0000

    backup for picture

--- settings.py
+++ settings.py
@@ -253,7 +253,7 @@ INSTALLED_APPS = (
     "mezzanine.forms",
     "mezzanine.pages",
     "mezzanine.galleries",
-    "mezzanine.twitter",
+    #"mezzanine.twitter",
     "mezzanine.accounts",
     # "mezzanine.mobile",
     "mezzanine_pagedown",
