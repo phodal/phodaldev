32f6cba4b495597c63ad57a60cd18e6e0d7d9e89
Fengda HUANG <h@phodal.com>
2015-03-15 08:15:47 +0800

    Test reuse newrelic

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 source /home/www/MK_dream/bin/activate
 killall gunicorn_django
-nohup /home/www/MK_dream/bin/gunicorn_django --workers=1 -b 0.0.0.0:9997 --timeout=300&
+NEW_RELIC_CONFIG_FILE=/home/www/MK_dream/MK_dream/newrelic.ini
+export NEW_RELIC_CONFIG_FILE
+nohup /home/www/MK_dream/bin/newrelic-admin run-program /home/www/MK_dream/bin/gunicorn_django --workers=2 -b 0.0.0.0:9997 --timeout=300&
