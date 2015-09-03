25c1c39c88b571d7bb3db3579a06e7e2c87dcac5
Fengda HUANG <h@phodal.com>
2015-09-03 10:36:29 +0800

    remove unused API

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-
 from api.blog_api import BlogpostListSet, BlogpostDetailSet, create_blog
-from api.views import AllListView
 
 router = routers.DefaultRouter()
 router.register(r'blog_list', BlogpostListSet)
 router.register(r'blog_detail', BlogpostDetailSet)
-
-router.register(r'all', AllListView, 'all')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
