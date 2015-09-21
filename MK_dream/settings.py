826a44382074ecb34c3d92ebdf683950ea80df7f
Fengda HUANG <h@phodal.com>
2015-09-22 07:57:34 +0800

    custom admin menu

--- MK_dream/settings.py
+++ MK_dream/settings.py
@@ -13,12 +13,12 @@ from __future__ import absolute_import, unicode_literals
 
 # Controls the ordering and grouping of the admin menu.
 #
-# ADMIN_MENU_ORDER = (
-#     ("Content", ("pages.Page", "blog.BlogPost",
-#        "generic.ThreadedComment", ("Media Library", "fb_browse"),)),
-#     ("Site", ("sites.Site", "redirects.Redirect", "conf.Setting")),
-#     ("Users", ("auth.User", "auth.Group",)),
-# )
+ADMIN_MENU_ORDER = (
+    ("Content", ("pages.Page", "blog.BlogPost",
+       "generic.ThreadedComment", ("Media Library", "fb_browse"),)),
+    ("Site", ("sites.Site", "redirects.Redirect", "conf.Setting", "events.Event", "events.Ad", "homepage.Info",)),
+    ("Users", ("auth.User", "auth.Group")),
+)
 
 # A three item sequence, each containing a sequence of template tags
 # used to render the admin dashboard.
