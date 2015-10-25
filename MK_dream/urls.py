6f7a9f744241751fa5a20abac4d4519919c5c0ec
Fengda HUANG <h@phodal.com>
2015-10-25 14:08:53 +0800

    use python wechat sdk

--- MK_dream/urls.py
+++ MK_dream/urls.py
@@ -57,7 +57,7 @@ urlpatterns += patterns('',
     url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^api-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
     url(r'^api-token-verify/', 'rest_framework_jwt.views.verify_jwt_token'),
-    url(r'^weixin/', 'weixin.views.wechat'),
+    url(r'^weixin', 'weixin.views.wechat'),
     url("^", include("mezzanine.urls")),
 )
 
--- weixin/views.py
+++ weixin/views.py
@@ -1,17 +1,67 @@
-from django.http import HttpResponse
-from wechat.models import WxTextResponse
-from wechat.official import WxApplication
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import TextMessage
+
+WECHAT_TOKEN = 'thisislooklikeatookenmaybe'
+AppID = 'wxd196ba4c0237a839'
+AppSecret = 'd760de12c8c2a71ef808f2d4837428ce'
+
+# 实例化 WechatBasic
+wechat_instance = WechatBasic(
+    token=WECHAT_TOKEN,
+    appid=AppID,
+    appsecret=AppSecret
+)
 
 
-class EchoApp(WxApplication):
-    SECRET_TOKEN = ''
-    APP_ID = ''
-    ENCODING_AES_KEY = ''
-
-    def on_text(req):
-        return WxTextResponse(req.Content, req)
-
-
+@csrf_exempt
 def wechat(request):
-    echo = EchoApp()
-    return HttpResponse(echo.process(request.GET, request.body))
+    if request.method == 'GET':
+        # 检验合法性
+        # 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+
+        if not wechat_instance.check_signature(
+                signature=signature, timestamp=timestamp, nonce=nonce):
+            return HttpResponseBadRequest('Verify Failed')
+
+        return HttpResponse(
+            request.GET.get('echostr', ''), content_type="text/plain")
+
+
+    # 解析本次请求的 XML 数据
+    try:
+        wechat_instance.parse_data(data=request.body)
+    except ParseError:
+        return HttpResponseBadRequest('Invalid XML Data')
+
+    # 获取解析好的微信请求信息
+    message = wechat_instance.get_message()
+
+    # 关注事件以及不匹配时的默认回复
+    response = wechat_instance.response_text(
+        content=(
+            '感谢您的关注！\n回复【功能】两个字查看支持的功能，还可以回复任意内容开始聊天！\nPhodal君正在实现功能中。'
+        ))
+    if isinstance(message, TextMessage):
+        # 当前会话内容
+        content = message.content.strip()
+        if content == '功能':
+            reply_text = (
+                '目前支持的功能：\n' +
+                'Phodal君正在实现功能中。'
+            )
+        else:
+            reply_text = '稍等：Phodal君正在实现功能中。'
+
+        response = wechat_instance.response_text(content=reply_text)
+
+    return HttpResponse(response, content_type="application/xml")
