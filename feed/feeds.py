0802678da7bbd31df4cadd65054c3522c27e5bfd
Fengda HUANG <h@phodal.com>
2016-03-25 17:00:30 +0800

    add wechat desc & fixed #36

--- feed/feeds.py
+++ feed/feeds.py
@@ -80,7 +80,7 @@ class PostsRSS(Feed):
     def item_description(self, item):
         copy_info = "\n原文:[" + \
                     super(PostsRSS, self).item_title(item) + "](" + \
-                    super(PostsRSS, self).item_link(item) + ")"
+                    super(PostsRSS, self).item_link(item) + ")" + "<br />更多精彩内容，欢迎搜索并关注我的微信公众号: Phodal"
         return richtext_filters(item.content[0:600]) + richtext_filters(copy_info)
 
     def categories(self):
