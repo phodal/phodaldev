a7ea289851198fb773d5bc8c5698b9e482f46a57
Fengda HUANG <h@phodal.com>
2016-03-27 23:13:01 +0800

    Add amp templates

--- amp/views.py
+++ amp/views.py
@@ -4,17 +4,17 @@ from mezzanine.blog.models import BlogPost
 
 
 def amp_blog_post_detail(request, slug, year=None, month=None, day=None,
-                     template="blog/blog_post_detail.html",
+                     template="blog/amp_blog_post_detail.html",
                      extra_context=None):
 
-    print(slug)
+    print(request)
     blog_posts = BlogPost.objects.published(
         for_user=request.user).select_related()
     blog_post = get_object_or_404(blog_posts, slug=slug)
     related_posts = blog_post.related_posts.published(for_user=request.user)
     context = {"blog_post": blog_post, "editable_obj": blog_post,
-               "related_posts": related_posts}
+               "related_posts": related_posts, "slug": slug}
     context.update(extra_context or {})
-    templates = [u"blog/blog_post_detail_%s.html" % str(slug), template]
+    templates = [u"blog/amp_blog_post_detail_%s.html" % str(slug), template]
     return TemplateResponse(request, templates, context)
 
