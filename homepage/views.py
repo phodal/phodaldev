0884ffaceb6df757c9497c4964c34d9181309b2c
Phodal HUANG <h@phodal.com>
2018-01-16 19:01:58 +0800

    [DJ2.0] fix homepage issue

--- homepage/views.py
+++ homepage/views.py
@@ -1,28 +1,22 @@
-from __future__ import unicode_literals
-
+from django.template.response import TemplateResponse
 from mezzanine.blog.models import BlogPost
 from mezzanine.conf import settings
-from mezzanine.utils.models import get_user_model
 from mezzanine.utils.views import paginate
-from mezzanine.utils.views import render
-
-User = get_user_model()
 
 
-def homepage(request, tag=None, year=None, month=None, username=None,
-             category=None, template="index.html"):
-    settings.use_editable()
-    templates = []
-    blog_posts = BlogPost.objects.published(for_user=request.user)
-    author = None
+def homepage(request, tag=None, year=None, month=None, category=None, template="index.html"):
+  settings.use_editable()
+  templates = []
+  blog_posts = BlogPost.objects.published(for_user=request.user)
+  author = None
 
-    prefetch = ("categories", "keywords__keyword")
-    blog_posts = blog_posts.select_related("user").prefetch_related(*prefetch)[:3]
-    blog_posts = paginate(blog_posts, request.GET.get("page", 1),
-                          settings.BLOG_POST_PER_PAGE,
-                          settings.MAX_PAGING_LINKS)
+  prefetch = ("categories", "keywords__keyword")
+  blog_posts = blog_posts.select_related("user").prefetch_related(*prefetch)[:3]
+  blog_posts = paginate(blog_posts, request.GET.get("page", 1),
+                        settings.BLOG_POST_PER_PAGE,
+                        settings.MAX_PAGING_LINKS)
 
-    context = {"blog_posts": blog_posts, "year": year, "month": month, "tag": tag, "category": category,
-               "author": author}
-    templates.append(template)
-    return render(request, templates, context)
+  context = {"blog_posts": blog_posts, "year": year, "month": month, "tag": tag, "category": category,
+             "author": author}
+  templates.append(template)
+  return TemplateResponse(request, templates, context)
