f201f50441912ad36e681ba9786bb54a1e68e8c0
Phodal HUANG <h@phodal.com>
2018-01-17 23:22:24 +0800

    [UI3.0] update markdown render

--- playdown/templatetags/playdown_tags.py
+++ playdown/templatetags/playdown_tags.py
@@ -1,6 +1,10 @@
 from django import template
 from mezzanine.blog.models import BlogPost
 from mezzanine.core.models import CONTENT_STATUS_DRAFT
+from markdown import markdown
+
+from playdown.plugins.progressiveimage import ProgressiveImageExtension
+from playdown.plugins.tables import BlockQuoteExtension
 
 register = template.Library()
 
@@ -21,3 +25,8 @@ def draft_blog_post_for(context, user):
 
     blog_posts = BlogPost.objects.all().filter(user_id=user.id, status=CONTENT_STATUS_DRAFT)
     return blog_posts
+
+@register.simple_tag(takes_context=True)
+def markit(context, content):
+    content = markdown(content, [ProgressiveImageExtension(), 'headerid', 'codehilite', 'extra', 'meta', BlockQuoteExtension()])
+    return content
