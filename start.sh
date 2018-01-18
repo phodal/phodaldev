111b6d483b56d8152208aa16e59d49791f392d82
Phodal <h@phodal.com>
2018-01-18 12:37:36 +0000

    [T] update build script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-source /home/www/MK_dream/bin/activate
+source /home/www/MK_dream/phodal-env/bin/activate
 killall gunicorn
 NEW_RELIC_CONFIG_FILE=/home/www/MK_dream/MK_dream/newrelic.ini
 export NEW_RELIC_CONFIG_FILE
 #nohup /home/www/MK_dream/bin/newrelic-admin run-program /home/www/MK_dream/bin/gunicorn --workers=2 MK_dream.wsgi -b 0.0.0.0:9997 --timeout=300& 
-nohup /home/www/MK_dream/bin/gunicorn --workers=2 MK_dream.wsgi -b 0.0.0.0:9997 --timeout=300&
+nohup /home/www/MK_dream/phodal-env/bin/gunicorn --workers=2 MK_dream.wsgi -b 0.0.0.0:9997 --timeout=300&
 
