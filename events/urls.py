c9dafe7da48d2ee08bfa2157e34e795a8847ec79
Phodal Huang <h@phodal.com>
2022-05-26 11:37:15 +0800

    fix: fix editor issues for frontend

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 
-from django.urls import path
+from django.urls import re_path
 
 from events import views as events_view
 
 urlpatterns = [
-    path("^", events_view.events_list, name="events_list"),
+    re_path("^", events_view.events_list, name="events_list"),
 ]
