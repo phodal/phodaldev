e4a71760f4ad1d21ba13845ab3840ba536ed4128
Phodal Huang <h@phodal.com>
2022-05-26 09:17:33 +0800

    upgrade(4.0): align settings and urls

--- MK_dream/settings.py
+++ MK_dream/settings.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, unicode_literals
 import os
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 ######################
 # MEZZANINE SETTINGS #
--- MK_dream/urls.py
+++ MK_dream/urls.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import include, url
+from django.conf.urls import include
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
+from django.urls import path
 
 from homepage import views as homepage_view
 from feed import view as feed_view
@@ -15,34 +16,34 @@ from rest_framework_jwt.views import verify_jwt_token, obtain_jwt_token, refresh
 admin.autodiscover()
 
 urlpatterns = i18n_patterns(
-  url("^admin/", include(admin.site.urls)),
+  path("^admin/", include(admin.site.urls)),
 )
 
 sitemaps = {"sitemaps": {"all": DisplayableSitemap}}
 mobile_sitemaps = {"sitemaps": {"all": DisplayableMobileSitemap}}
 
 urlpatterns += [
-  url("^sitemap\.xml$", sitemap.sitemap, sitemaps),
-  url("^sitemap_mobile\.xml$", sitemap.mobile, sitemaps),
+  path("^sitemap\.xml$", sitemap.sitemap, sitemaps),
+  path("^sitemap_mobile\.xml$", sitemap.mobile, sitemaps),
 ]
 
 urlpatterns += [
-  url("feeds/(?P<format>.*)/$", feed_view.blog_post_feed, name="blog_post_feed"),
-  url("^blog/feeds/(?P<format>.*)/$", feed_view.blog_post_feed, name="blog_post_feed")
+  path("feeds/(?P<format>.*)/$", feed_view.blog_post_feed, name="blog_post_feed"),
+  path("^blog/feeds/(?P<format>.*)/$", feed_view.blog_post_feed, name="blog_post_feed")
 ]
 
 urlpatterns += [
-  url("^events/", include("events.urls")),
+  path("^events/", include("events.urls")),
 ]
 
 urlpatterns += [
-  url("^$", homepage_view.homepage, name="home"),
-  url("^amp/(?P<slug>.*)/$", amp_views.amp_blog_post_detail, name="blog_post_detail"),
-  url(r'^api/app/', include("api.urls")),
-  url(r'^api-token-auth/', obtain_jwt_token),
-  url(r'^api-token-refresh/', refresh_jwt_token),
-  url(r'^api-token-verify/', verify_jwt_token),
-  url("^", include("mezzanine.urls")),
+  path("^$", homepage_view.homepage, name="home"),
+  path("^amp/(?P<slug>.*)/$", amp_views.amp_blog_post_detail, name="blog_post_detail"),
+  path(r'^api/app/', include("api.urls")),
+  path(r'^api-token-auth/', obtain_jwt_token),
+  path(r'^api-token-refresh/', refresh_jwt_token),
+  path(r'^api-token-verify/', verify_jwt_token),
+  path("^", include("mezzanine.urls")),
 ]
 
 handler404 = "mezzanine.core.views.page_not_found"
